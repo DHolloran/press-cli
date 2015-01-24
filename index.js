388c7fffc4de5d91103b62a287cc1d1400b98902
Dan Holloran <dtholloran@gmail.com>
2015-01-24 07:49:58 -0600

    Removes some debug statements

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var sn = require('./libs/sn/init.js');
-sn.init();
 
-console.log( 'Directory Name: ' + sn.options.config.install.directoryName );
-console.log( 'dbname: ' + sn.options.config.install.dbname );
-console.log( 'dbprefix: ' + sn.options.config.install.dbprefix );
-console.log( 'Theme Name: ' + sn.options.config.newTheme.themeName );
-console.log( 'Theme URL: ' + sn.options.config.newTheme.themeURL );
-console.log( 'Theme Description: ' + sn.options.config.newTheme.themeDescription );
