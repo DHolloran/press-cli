22c9340998290f99a933ec8b57e50c83452ea1f1
Dan Holloran <dtholloran@gmail.com>
2016-09-24 19:21:52 -0500

    Tweaks configuration

--- example.config.php
+++ example.config.php
@@ -9,20 +9,29 @@ $config = [
         'host' => '127.0.0.1:8889',
     ],
     'plugins' => [
-        [ 'plugin' => 'jetpack',
-            'activate' => false
+        [
+            'plugin' => 'jetpack',
+            'activate' => false,
         ],
-        [ 'plugin' => 'crop-thumbnails',
-            'activate' => true
+        [
+            'plugin' => 'crop-thumbnails',
+            'activate' => true,
         ],
-        [ 'plugin' => 'query-monitor',
-            'activate' => true
+        [
+            'plugin' => 'query-monitor',
+            'activate' => true,
         ],
-        [ 'plugin' => 'force-regenerate-thumbnails',
-            'activate' => true
+        [
+            'plugin' => 'force-regenerate-thumbnails',
+            'activate' => true,
         ],
-        [ 'plugin' => 'force-regenerate-thumbnails',
-            'activate' => true
+        [
+            'plugin' => 'force-regenerate-thumbnails',
+            'activate' => true,
+        ],
+        [
+            'plugin' => 'disable-comments',
+            'activate' => false,
         ],
         [
             'plugin' => '~/Desktop/plugins/advanced-custom-fields.zip',
@@ -45,15 +54,19 @@ $config = [
         'email' => 'dholloran@matchboxdesigngroup.com',
         'password' => 'jooL8bYGrYdkVyHKvAeFDsiBnyEB9y',
     ],
-    // 'theme' => [
-    //     'zip' => '',
-    //     'root' => '/',
-    //     'name' => 'test-theme',
-        // Update style.css
-    // ],
+    'theme' => [
+        'type' => 'zip', // zip, tar, git
+        'url' => '',
+        'root' => '/',
+        'name' => 'test-theme',
+        'style-css' => [
+            // @todo allow for 'variable' => 'Something' to rewrite {{variable}} in style.css.
+        ],
+    ],
     'site' => [
         'title' => 'Site Title',
         'url' => 'test.dev',
+        'client' => 'Test Client',
     ],
     'commands' => [
         'postInstall' => [
