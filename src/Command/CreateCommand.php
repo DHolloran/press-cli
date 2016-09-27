e6b22675302744cd9794904dcef4fdfcd237df31
Dan Holloran <dtholloran@gmail.com>
2016-09-26 19:29:08 -0500

    Renames init ro create

--- src/Command/CreateCommand.php
+++ src/Command/CreateCommand.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class InitCommand extends Command
+class CreateCommand extends Command
 {
     /**
      * Configure create command.
