4b1d4b5a01d521e12e89daee3803f0aca73a94dd
Dan Holloran <dtholloran@gmail.com>
2016-10-15 11:08:24 -0500

    Fixes WP CLI path when installing globally

--- src/WPCLI/CLI.php
+++ src/WPCLI/CLI.php
@@ -16,7 +16,7 @@ class CLI
     {
         $arguments = trim(implode(' ', $arguments));
         $options = self::execOptions($options);
-        $command = trim(KCLI_EXEC_DIR . "/vendor/bin/wp {$command}");
+        $command = trim(KCLI_WP_EXEC . " {$command}");
         $command = implode(' ', array_filter([$command, $arguments, $options]));
 
         return system($command);
